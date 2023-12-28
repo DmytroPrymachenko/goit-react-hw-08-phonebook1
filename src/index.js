0c863cc0d109e287b537d4d3052f476eaeacea7e
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-12-28 15:07:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import App from './components/app/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-08-phonebook">
+    <BrowserRouter basename="goit-react-hw-08-phonebook1">
       <Provider store={store}>
         <App />
         <ToastContainer />
