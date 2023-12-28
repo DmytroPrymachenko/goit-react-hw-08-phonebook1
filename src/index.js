1624fa6854dc548f3d0ddaf5c06696ff4ff096cf
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-12-28 14:41:53 +0200

    2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import App from './components/app/App';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
