511adbb5831b7fe8915772aa1ae1f922f7bb909b
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-12-28 14:55:01 +0200

    6

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import UserButton from '../Button';
+import UserButton from '../button/Button';
 import {
   Header,
   HeaderNavLink,
